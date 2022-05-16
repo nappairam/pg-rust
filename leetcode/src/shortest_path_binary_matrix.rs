510e985a99683f0b9cc779581a45bc65b8e75ace
Mariappan Ramasamy <1221719+nappairam@users.noreply.github.com>
2022-05-17 02:26:52 +0800

    Add solution for binary matrix shortest path

--- leetcode/src/shortest_path_binary_matrix.rs
+++ leetcode/src/shortest_path_binary_matrix.rs
@@ -16,28 +16,44 @@
 // 1 <= n <= 100
 // grid[i][j] is 0 or 1
 
-use std::collections::{HashSet, VecDeque};
-
 struct Solution;
 
-const OFFSETS: [(i32, i32); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+use std::collections::{VecDeque};
+
+const DIRECTIONS: [(i32, i32); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
 
 impl Solution {
-    pub fn shortest_path_binary_matrix(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn shortest_path_binary_matrix(mut grid: Vec<Vec<i32>>) -> i32 {
         let grid_length = grid.len();
-        let mut visited = HashSet::new();
-        let mut queue = VecDeque::with_capacity(grid_length);
-
-        queue.push_back((0, 0, 1));
-        visited.insert(1);
-
         // Handle corner case
         if grid[0][0] == 1 || grid[grid_length-1][grid_length-1] == 1 {
             return -1;
         }
 
+        let mut queue = VecDeque::with_capacity(grid_length * grid_length);
 
-        0
+        grid[0][0] = 1;
+        queue.push_back((0, 0));
+
+
+        while !queue.is_empty() {
+            let (x, y) = queue.pop_front().unwrap();
+            let is_safe_index = | i | i >= 0 && i < grid_length as i32;
+            let is_safe = |x: i32, y: i32| is_safe_index(x) && is_safe_index(y);
+
+            for (next_x, next_y) in DIRECTIONS.iter()
+                .map(|(_x, _y)| (x+_x, y+_y))
+                .filter(|&(_x, _y)| is_safe(_x, _y))
+            {
+                if grid[next_x as usize][next_y as usize] != 0 {
+                    continue;
+                }
+                grid[next_x as usize][next_y as usize] = grid[x as usize][y as usize] + 1;
+                queue.push_back((next_x, next_y));
+            }
+        }
+
+        if 0 != grid[grid_length-1][grid_length-1] {grid[grid_length-1][grid_length-1]} else {-1}
     }
 }
 
@@ -48,6 +64,18 @@ mod tests {
     #[test]
     fn test_shortest_path() {
         let grid = vec![vec![0, 1], vec![1, 0]];
-        assert_eq!(Solution::shortest_path_binary_matrix(grid), 0);
+        assert_eq!(Solution::shortest_path_binary_matrix(grid), 2);
+        let grid = vec![vec![0, 0, 0], vec![1, 1, 0], vec![1, 1, 0]];
+        assert_eq!(Solution::shortest_path_binary_matrix(grid), 4);
+    }
+    #[test]
+    fn test_shortest_path_with_no_starting_point() {
+        let grid = vec![vec![1, 0, 0], vec![1, 1, 0], vec![1, 1, 0]];
+        assert_eq!(Solution::shortest_path_binary_matrix(grid), -1);
+    }
+    #[test]
+    fn test_shortest_path_no_path() {
+        let grid = vec![vec![0, 1, 0], vec![1, 1, 0], vec![1, 1, 0]];
+        assert_eq!(Solution::shortest_path_binary_matrix(grid), -1);
     }
 }
